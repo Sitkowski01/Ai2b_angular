49d38a7d98c85d3cf739fbc2b8533ce534534d2d
sm49395 <sm49395@zut.edu.pl>
2023-12-02 17:48:36 +0100

    lab-d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { DetailsComponent } from './details/details.component';
-import { AddPersonComponent } from './add-person/add-person.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { TasksComponent } from './tasks/tasks.component';
+import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    DetailsComponent,
-    AddPersonComponent,
-    NotFoundComponent
+    TasksComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
